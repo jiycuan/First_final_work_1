4e3bdea96e05863003e96173d6f2d8eefd8921ce
Сорокин Алексей <jiysuan@mail.ru>
2022-09-04 17:41:40 +0400

    Написал код к заданию

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше, либо равна 3 символа.
+
+void FirstFinalWork1()
+{
+    string[] FirstArray = {":-)",";-)",":-(",":-))","^_^",">_<",":3"};
+    string[] SecondArray = new string[FirstArray.Length];
+
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if (FirstArray[i].Length < 4)
+        {
+            SecondArray[i] = FirstArray[i];
+        }
+    }
+
+    Console.Write("[ ");
+    for (int i = 0; i < SecondArray.Length; i++)
+    {
+        Console.Write(SecondArray[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+FirstFinalWork1();
+
+/* 
+*/
